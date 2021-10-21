d08546fc0528573a0fcd15c1cead9729ab450ef3
Ian Gulyas <IanGulyas@gmail.com>
2021-10-21 15:36:48 -0400

    started test for attept procesor

---
+++ src/test/java/obg/AttemptProcescerTest.java
@@ -0,0 +1,15 @@
+package obg;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AttemptProcescerTest {
+    @Test
+    public void validStudent(){
+        String testUname = "Dave";
+
+    }
+
+
+}
