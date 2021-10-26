8a91f9de8a7d2e1c1d853c36505e97cd334e6272
jacksonmckeigue <mckeiguej24@hanover.edu>
2021-10-26 15:01:00 -0400

    Created empty objectiveSummary class.

--- src/main/java/obg/ObjectiveMap.java
+++ src/main/java/obg/ObjectiveMap.java
@@ -4,24 +4,26 @@ import java.util.HashMap;
 
 public class ObjectiveMap {
 
+    private static String studentId;
     private static String[] obj;
+    private static String getStudentId;
     int grade;
 
     public static void main(String[] args) {
+        String[][] getObjectiveAndGrade = {{"L1", "4"}, {"S1", "2"}};
         ObjectiveMap.obj = args;
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, String[][]> map = new HashMap<>();
 
-        map.put("L1", null);
-        map.put("L2", null);
-        map.put("L3", null);
+        map.put(getStudentId, getObjectiveAndGrade);
+        map.put(getStudentId, getObjectiveAndGrade);
+        map.put(getStudentId, getObjectiveAndGrade);
 
         //System.out.println("Size of map is: " + map.size());
         System.out.println(map);
 
     }
 
-    public int getGrade(){
-        return grade;
-    }
+    public int getGrade(){return grade;}
 
+    public String getStudentId() {return studentId;}
 }
---
+++ src/test/java/obg/ObjectiveMapTest.java
@@ -0,0 +1,10 @@
+package obg;
+
+import junit.framework.TestCase;
+
+public class ObjectiveMapTest extends TestCase {
+
+    public void testObjectiveMap() {
+        ObjectiveMap map = new ObjectiveMap();
+    }
+}
