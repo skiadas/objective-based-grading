d30a3d0be1fd0427ed06aa7843e3b47636b29d25
Haris Skiadas <cskiadas@gmail.com>
2021-10-27 13:19:58 -0400

    Work on basic index and login pages

--- src/main/java/webserver/Server.java
+++ src/main/java/webserver/Server.java
@@ -1,18 +1,63 @@
 package webserver;
 
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
 import spark.Spark;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 
 public class Server {
     private static Logger logger = LogManager.getLogger();
+    private static ThymeleafTemplateEngine engine = new ThymeleafTemplateEngine();
 
     public static void main(String[] args) {
         before("/*", (req, res) -> logger.info("Received call: " + req.pathInfo()));
-        get("/", (req, res) -> "Hello there!");
+        get("/", Server::handleIndex);
+        get("/login", Server::showLoginScreen);
+        post("/login", Server::handleLoginRequest);
+        post("/logout", Server::handleLogout);
         logger.info("Server started, serving at localhost:" + Spark.port());
+    }
+
+    private static Object handleLogout(Request req, Response res) {
+        req.session().attribute("user", null);
+        res.redirect("/");
+        return null;
+
+    }
+
+    private static Object handleLoginRequest(Request req, Response res) {
+        String username = req.queryParams("username");
+        req.session().attribute("user", new User(username));
+        res.redirect("/");
+        return null;
+    }
+
+    private static Object showLoginScreen(Request req, Response res) {
+        return engine.render(new ModelAndView(new HashMap<>(), "login"));
+    }
+
+    private static Object handleIndex(Request req, Response res) {
+        // TODO: What to show on index page
+        User user = retrieveUser(req);
+        if (user == null) {
+            res.redirect("/login");
+            return null;
+        }
+        Map<String, Object> model = new HashMap<>();
+        model.put("user", user);
+        model.put("message", "Hi!");
+        return engine.render(new ModelAndView(model, "index"));
+    }
+
+    private static User retrieveUser(Request req) {
+        return req.session().attribute("user");
     }
 }
